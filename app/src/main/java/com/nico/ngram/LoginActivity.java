ad326edb48ca8b699d1161c725d8bedc93122095
nicohe <h.nico@hotmail.com.ar>
2016-11-14 20:44:39 -0300

    Initial commit

---
+++ app/src/main/java/com/nico/ngram/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.nico.ngram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.nico.ngram.view.ContainerActivity;
+import com.nico.ngram.view.CreateAccountActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void goCreateAccount(View view) {
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+    public void goHome(View view){
+        Intent intent = new Intent(this, ContainerActivity.class);
+        startActivity(intent);
+    }
+}
