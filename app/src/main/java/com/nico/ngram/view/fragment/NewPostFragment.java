a51c68897a6a9cb8568e1638ad9519d32bdc2643
nicohe <h.nico@hotmail.com.ar>
2016-12-16 15:26:37 -0300

    feature take photo

---
+++ app/src/main/java/com/nico/ngram/view/fragment/NewPostFragment.java
@@ -0,0 +1,116 @@
+package com.nico.ngram.view.fragment;
+
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.support.v7.view.menu.MenuView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.nico.ngram.R;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NewPostFragment extends Fragment {
+
+    ImageView ivPicture;
+    Button btnTakePicture;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
+
+    public NewPostFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_new_post, container, false);
+
+        ivPicture = (ImageView) view.findViewById(R.id.ivPicture);
+        btnTakePicture = (Button) view.findViewById(R.id.btnTakePicture);
+
+        btnTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
+
+        return view;
+    }
+
+    private void takePicture() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null){
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if(photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getActivity(), "com.nico.ngram", photoFile);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK) {
+
+            Picasso.with(getActivity()).load(mCurrentPhotoPath).into(ivPicture);
+            addPictureToGallery();
+            Toast.makeText(getActivity(), mCurrentPhotoPath, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private File createImageFile() throws IOException{
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd__HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+
+        return image;
+    }
+
+    private void addPictureToGallery() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File newFile = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(newFile);
+        mediaScanIntent.setData(contentUri);
+        getActivity().sendBroadcast(mediaScanIntent);
+    }
+
+}
